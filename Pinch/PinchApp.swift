3f00bbf391094c3d4d804a4d7631dbbead7b484f
Yashwant Das <yashworks@gmail.com>
2022-02-19 22:19:01 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Yashwant Das on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
